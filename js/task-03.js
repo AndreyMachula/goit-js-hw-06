681d22cb16ffe1fd8d58159882feed6e22f01bc6
AndreyMachula <amcz2020@gmail.com>
2022-04-24 23:11:40 +0200

    goit-hw-6-task-3

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,38 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+/*Напиши скрипт для создания галереи изображений по массиву данных.
+Используй массив объектов images для создания элементов <img> вложенных в <li>. 
+Для создания разметки используй шаблонные строки и метод insertAdjacentHTML().
+ • Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+ • Добавь минимальное оформление галереи флексбоксами или гридами через CSS классы.*/
+
+const galleryEl = document.querySelector('.gallery');
+
+const imgEl = images
+  .map((image) => `<li><img src="${image.url}" alt="${image.alt}"></li>`)
+  .join('');
+console.log(imgEl);
+
+galleryEl.insertAdjacentHTML('beforeend', imgEl); 
+console.log(galleryEl);
+
+//================================================================
+
+// for (let el of images) document.querySelector('.gallery')
+//   .insertAdjacentHTML('beforeEnd', `<li><img src="${el.url}" alt="${el.alt}"></li>`);
+// console.log(images);  
+  
+//================================================================
+ 
+// const galleryEl = document.querySelector('.gallery');
+
+// images.forEach(image => {
+//   galleryEl.insertAdjacentHTML(
+//     'beforeend',
+//     `<li><img src="${image.url}" alt="${image.alt}"></li>`);
+//   });
+    
+// console.log(galleryEl);
+
