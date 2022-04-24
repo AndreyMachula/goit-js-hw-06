596b85209bd7d68904b789a62d1c5d1db5f12965
AndreyMachula <amcz2020@gmail.com>
2022-04-24 23:12:24 +0200

    goit-hw-6-task-5

---
+++ js/task-05.js
@@ -0,0 +1,14 @@
+/*Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input), 
+подставляет его текущее значение в span#name-output. Если инпут пустой, в спане должна 
+отображаться строка "Anonymous".
+*/
+
+const textInput = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
+// console.log(textInput);
+// console.log(output);
+
+textInput.addEventListener("input", (event) => {
+  output.textContent = event.currentTarget.value;
+});
+// console.log(output.textContent);
